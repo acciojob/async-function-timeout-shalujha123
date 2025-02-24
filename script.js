3fd9906785579964650c01bc711a087c43a898df
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-24 12:43:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let form = document.querySelector("form");
+      let text = document.getElementById("text");
+      let delay = document.getElementById("delay");
+      let output = document.getElementById("output");
+      let btn = document.getElementById("btn");
+
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        setTimeout(async () => {
+          output.innerHTML = await text.value;
+        }, delay.value);
+        // form.reset()
+      });
